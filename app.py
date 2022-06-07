c45cacddc88dcb1a381b445d4bd382d258c6f9f4
Gabriel Cavedal <gcavedal@gmail.com>
2022-06-07 12:39:10 -0300

    video_list_as_json function to populate html table

--- app.py
+++ app.py
@@ -1,19 +1,38 @@
+from logging import root
 import os
 
-import cv2 as cv
 from flask import Flask, render_template
+
+from folders import VIDEOS_PATH as root_videos_dir
+
+
 app = Flask(__name__)
 
-from folders import VIDEOS_PATH as videos_dir 
 
-
+# Routes
 @app.route('/')
 def home():
     return render_template('home.html')
 
+
 @app.route('/vid')
 def videos():
-    return render_template('videos.html')
+    return render_template('videos.html', vids=video_list_as_json())
+
+
+# Functions
+def video_list_as_json():
+    all_videos = []
+    for folder in os.listdir(root_videos_dir):
+        # each folder should indetify a Raspberry Pi
+        video_path = os.path.join(root_videos_dir, folder)
+        rpi_vids = {}
+        for video in os.listdir(video_path):
+            rpi_vids['device'] = folder
+            rpi_vids['tag'] = video.split(sep='.')[0]
+            rpi_vids['src'] = "/".join(['videos', folder, video])
+            all_videos.append(rpi_vids)
+    return all_videos
 
 
 if __name__ == "__main__":
--- folders.py
+++ folders.py
@@ -1,4 +1,4 @@
 from pathlib import Path
 
 BASE_PATH = Path(__file__).parent.resolve()
-VIDEOS_PATH = BASE_PATH / 'videos'
+VIDEOS_PATH = BASE_PATH / 'static' / 'videos'
