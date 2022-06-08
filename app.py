e8444937dcb939f45761736c4ba8f3b8ec8ba8e3
Gabriel Cavedal <gcavedal@gmail.com>
2022-06-08 16:41:12 -0300

    first draw of WTForm to upload files

--- app.py
+++ app.py
@@ -1,23 +1,50 @@
+from crypt import methods
 from logging import root
 import os
 
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField
+from werkzeug.utils import secure_filename
+
 
 from folders import VIDEOS_PATH as root_videos_dir
 
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'Super Secure Password (not really...)'
+
+
+# Forms
+class Converter(FlaskForm):
+    file = FileField()
 
 
 # Routes
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def home():
     return render_template('home.html')
 
 
+@app.route('/converter', methods=['GET', 'POST'])
+def converter():
+    """ converts a JPG into a MP4 movie that last a setted time """
+    form = Converter()
+
+    if form.validate_on_submit():
+        filename = secure_filename(form.file.data.filename)
+        form.file.data.save('converter/' + filename)
+        return redirect(url_for('home'))
+
+    return render_template('converter.html', form=form)
+
+
 @app.route('/vid')
 def videos():
-    return render_template('videos.html', all_content=video_list_as_json())
+    content = {
+        "all_devices": video_list_as_json()
+    }
+    return render_template('videos.html', **content)
 
 
 # Functions
@@ -38,13 +65,15 @@ def video_list_as_json():
         video_path = os.path.join(root_videos_dir, folder)
         position = 0
         rpi_id = {}
-        for video in os.listdir(video_path):    
-            position += 1
-            rpi_vid = {}
-            rpi_vid['tag'] = video.split(sep='.')[0]
-            rpi_vid['src'] = "/".join(['videos', folder, video])
-            rpi_id[position] = rpi_vid
-        rpi[folder] = rpi_id
+        if os.path.isdir(video_path):
+            for video in os.listdir(video_path):
+                position += 1
+                rpi_vid = {}
+                rpi_vid['tag'] = video.split(
+                    sep='.')[0].replace('_', ' ').lower()
+                rpi_vid['src'] = "/".join(['videos', folder, video])
+                rpi_id[position] = rpi_vid
+            rpi[folder] = rpi_id
     return rpi
 
 
